0ef0a958d56f50ab7963d1d1f973f0e28621fcf6
tom2youngblood <tom2youngblood@gmail.com>
2017-02-10 12:18:08 -0500

    using oop as a spacer

--- main_clicker_heroes.java
+++ main_clicker_heroes.java
@@ -3,21 +3,16 @@ public class main_clicker_heroes {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome To Clicker Heroes");
-		spacer.spacerDoSomething();
+		spacer space = new spacer();
+		space.spacerDoSomething();
     System.out.println("Whats your name?:");
+		space.spacerDoSomething();
     Scanner scan = new Scanner(System.in);
     String name = scan.next();
     System.out.println("Your name is: " + name + "!");
-		spacer.spacerDoSomething();
+		space.spacerDoSomething();
 
 
 	}
 }
 
-public class spacer {
-     public static void spacerDoSomething(){
-			System.out.println(" ")
-   	 	System.out.println("-------------------------");
-			System.out.println(" ")
-     }
-}
---
+++ spacer.java
@@ -0,0 +1,12 @@
+public class spacer {
+ 
+	public spacer (){
+		//empty	
+	}
+	
+	public void spacerDoSomething(){
+			System.out.println(" ");
+   	 	System.out.println("-------------------------");
+			System.out.println(" ");
+     }
+}
