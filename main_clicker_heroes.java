a2846d1a576798c2d35e28838cb1bead9ab1cd1b
tom2youngblood <tom2youngblood@gmail.com>
2017-02-10 12:21:39 -0500

    updates

--- main_clicker_heroes.java
+++ main_clicker_heroes.java
@@ -1,10 +1,14 @@
 import java.util.Scanner; 
 public class main_clicker_heroes {
 	public static void main(String[] args) {
+		
+		//define spacers and other methods here
+		space.spacerDoSomething();
+	
+		//code so far and name input
 		spacer space = new spacer();
 		System.out.println("Welcome To Clicker Heroes");
 		spacer space = new spacer();
-		space.spacerDoSomething();
     System.out.println("Whats your name?:");
 		space.spacerDoSomething();
     Scanner scan = new Scanner(System.in);
