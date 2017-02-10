bdeee5ea1a9ca971f87c17e901d32cd714cf1e2a
tom2youngblood <tom2youngblood@gmail.com>
2017-02-10 11:19:47 -0500

    new_java_project

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; 
+public class clicker_heroes {
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Clicker Heroes");
+    System.out.println("-------------------------");
+    System.out.println("Whats your name?:")
+    Scanner scan = new Scanner(System.in);
+    new.next();
+
+	}
+}
